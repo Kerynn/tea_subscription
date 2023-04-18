9404e2667c6a5a0f9c406c99c81a19481e862df2
Kerynn Davis <kerynn.davis@gmail.com>
2023-04-18 15:10:17 -0700

    create subscriptions and relationships

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_18_220348) do
+ActiveRecord::Schema.define(version: 2023_04_18_220747) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,19 @@ ActiveRecord::Schema.define(version: 2023_04_18_220348) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "subscriptions", force: :cascade do |t|
+    t.string "title"
+    t.float "price"
+    t.string "status"
+    t.string "frequency"
+    t.bigint "tea_id"
+    t.bigint "customer_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["customer_id"], name: "index_subscriptions_on_customer_id"
+    t.index ["tea_id"], name: "index_subscriptions_on_tea_id"
+  end
+
   create_table "teas", force: :cascade do |t|
     t.string "title"
     t.string "description"
@@ -33,4 +46,6 @@ ActiveRecord::Schema.define(version: 2023_04_18_220348) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "subscriptions", "customers"
+  add_foreign_key "subscriptions", "teas"
 end
